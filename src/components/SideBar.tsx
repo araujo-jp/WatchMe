1a8ef396500eb4f922d1f741bdcf6d4e1569e000
Joao Paulo Araujo <paulointer077@gmail.com>
2021-09-17 17:37:45 -0400

    refactor: movie listing page

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from '../App';
+import '../styles/sidebar.scss';
+
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: Array<GenreResponseProps>
+  selectedGenreId: number;
+  setSelectedGenreId: (id: number) => void;
+}
+
+export function SideBar({genres, selectedGenreId, setSelectedGenreId}: SideBarProps) {
+  
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+   </nav>
+  )
 }
